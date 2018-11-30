6ed4e8c47a3d230288b26a9fd9a4b313e7418f51
Mara Bos <m-ou.se@m-ou.se>
2018-11-30 21:09:23 +0100

    Update example to also use the uorb module.

--- example/main.rs
+++ example/main.rs
@@ -1,11 +1,16 @@
 extern crate log;
 extern crate px4;
 
-use log::{warn, info};
-use px4::{info_raw, px4_module_main};
+use log::info;
+use px4::{info_raw, px4_message, px4_module_main};
+
+use px4::uorb::{Publish, Subscribe};
+
+#[px4_message("example/msg/debug_value.msg")]
+pub struct debug_value;
 
 #[px4_module_main]
-pub fn main(args: &[&str]) -> i32 {
+fn main(args: &[&str]) -> i32 {
 	info!("Hello World!");
 
 	info_raw!("\n |> \\/ /_|    |> | | /_ |_");
@@ -13,11 +18,23 @@ pub fn main(args: &[&str]) -> i32 {
 
 	info!("Arguments: {:?}", &args[1..]);
 
-	if args.get(1) == Some(&"panic") {
-		panic!("A disaster happened!");
-	}
+	info!("Publishing data...");
 
-	warn!("The end is near!");
+	let mut d = debug_value {
+		timestamp: 123,
+		value: 1.0f32,
+		ind: 13,
+	};
+	let p = d.advertise().unwrap();
+
+	d.timestamp = 456;
+	p.publish(&d).unwrap();
+
+	info!("debug_value exists: {}", debug_value::exists(0));
+	info!("debug_value group_count: {}", debug_value::group_count());
+
+	let sub = debug_value::subscribe().unwrap();
+	info!("Subscribed and read: {:?}", sub.get());
 
 	0
 }
