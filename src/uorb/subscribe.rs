5f907024a91c9388fcc2176e5f7ca755137025eb
Mara Bos <m-ou.se@m-ou.se>
2018-12-01 13:34:37 +0100

    Add documentation on subscribing.

--- src/uorb/subscribe.rs
+++ src/uorb/subscribe.rs
@@ -2,11 +2,22 @@ use super::{Message, c};
 use std::marker::PhantomData;
 use std::mem::uninitialized;
 
+/// A subscription to a [`Message`](trait.Message.html) topic.
+///
+/// Create one using
+/// [`subscribe`](trait.Subscribe.html#tymethod.subscribe) or
+/// [`subscribe_multi`](trait.Subscribe.html#tymethod.subscribe_multi).
+///
+/// [`Drop`](struct.Subscription.html#impl-Drop)ping the subscription will
+/// unsubscribe from the topic.
 pub struct Subscription<T> {
 	handle: i32,
 	phantom: PhantomData<Fn() -> T>,
 }
 
+/// Functions related to subscribing to message topics.
+///
+/// They are automatically implemented on all [`Message`](trait.Message.html)s.
 pub trait Subscribe {
 	fn exists(instance: u32) -> bool;
 	fn group_count() -> u32;
@@ -56,15 +67,18 @@ impl<T> Drop for Subscription<T> {
 }
 
 impl<T: Message> Subscription<T> {
-	pub fn raw_handle(&self) -> i32 {
-		self.handle
-	}
+
+	/// Get a copy of the latest message.
 	pub fn get(&self) -> Result<T, i32> {
 		unsafe {
 			let mut val = uninitialized::<T>();
 			self.copy(&mut val).map(|_| val)
 		}
 	}
+
+	/// Copy the latest message into the given message object.
+	///
+	/// It is safe for `*val` to be uninitialized when calling this function.
 	pub fn copy(&self, val: &mut T) -> Result<(), i32> {
 		assert_eq!(std::mem::size_of::<T>(), T::metadata().size() as usize);
 		unsafe {
@@ -76,6 +90,8 @@ impl<T: Message> Subscription<T> {
 			}
 		}
 	}
+
+	/// Check if there have been any updates since the last call to `get` or `copy`.
 	pub fn check(&self) -> Result<bool, i32> {
 		unsafe {
 			let mut updated = false;
@@ -87,6 +103,8 @@ impl<T: Message> Subscription<T> {
 			}
 		}
 	}
+
+	/// Get the timestamp of the latest message, in microseconds.
 	pub fn stat(&self) -> Result<u64, i32> {
 		unsafe {
 			let mut time = 0u64;
@@ -98,6 +116,8 @@ impl<T: Message> Subscription<T> {
 			}
 		}
 	}
+
+	/// Get the priority of the topic.
 	pub fn get_priority(&self) -> Result<i32, i32> {
 		unsafe {
 			let mut priority = 0i32;
@@ -109,6 +129,8 @@ impl<T: Message> Subscription<T> {
 			}
 		}
 	}
+
+	/// Set the minimum time (in milliseconds) between updates as seen by this subscription.
 	pub fn set_interval(&self, interval: u32) -> Result<(), i32> {
 		unsafe {
 			let r = c::orb_set_interval(self.handle, interval);
@@ -119,6 +141,8 @@ impl<T: Message> Subscription<T> {
 			}
 		}
 	}
+
+	/// Get the minimum time (in milliseconds) between updates as seen by this subscription.
 	pub fn get_interval(&self) -> Result<u32, i32> {
 		unsafe {
 			let mut interval = 0u32;
@@ -129,5 +153,10 @@ impl<T: Message> Subscription<T> {
 				Err(r)
 			}
 		}
+	}
+
+	/// Get the raw handle.
+	pub fn raw_handle(&self) -> i32 {
+		self.handle
 	}
 }
